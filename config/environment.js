16de57deb85523ec8a57260ccadc0acc25c8ebef
RedTn <redmond.tran@gmail.com>
2017-08-02 22:32:36 -0700

    Upgrade to ember-cli 2.14

--- config/environment.js
+++ config/environment.js
@@ -1,9 +1,10 @@
 /* eslint-env node */
+'use strict';
 
 module.exports = function(environment) {
-  var ENV = {
+  let ENV = {
     modulePrefix: 'ember-quickstart',
-    environment: environment,
+    environment,
     rootURL: '/',
     locationType: 'auto',
     EmberENV: {
